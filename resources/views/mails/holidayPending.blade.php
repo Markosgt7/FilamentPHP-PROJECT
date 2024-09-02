e19a463b2d2763f7e2a5390bc33282d1de192079
Markosgt7 <markos.guate7@gmail.com>
2024-09-02 12:49:24 -0600

    vista para solicitud de vacaciones por correo

---
+++ resources/views/mails/holidayPending.blade.php
@@ -0,0 +1,4 @@
+<h1>SOLICITUD DE VACACIONES</h1>
+
+<p>El empleado {{ $data['name']}} con correo {{ $data['email']}} ha solicitado el siguiente dia para vacaciones</p>
+<p>la siguiente fecha: <strong> {{ $data['day']}}</strong></p>
