4ef5de8a2bc8c6037e6ed8173367cca9dba992c3
Markosgt7 <markos.guate7@gmail.com>
2024-09-02 12:45:29 -0600

    actualizacion de solicitud de vacaciones

--- app/Filament/Resources/HolidayResource/Pages/EditHoliday.php
+++ app/Filament/Resources/HolidayResource/Pages/EditHoliday.php
@@ -3,8 +3,12 @@
 namespace App\Filament\Resources\HolidayResource\Pages;
 
 use App\Filament\Resources\HolidayResource;
+use App\Mail\HolidayApproved;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
 
 class EditHoliday extends EditRecord
 {
@@ -15,5 +19,31 @@ class EditHoliday extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        $record->update($data);
+
+        // send email if approved
+        if ($record->type === 'approved') {
+            $user = User::find($record->user_id);
+            $data = array(
+                'name' => $user->name,
+                'email' => $user->email,
+                'day' => $record->day,
+            );
+            Mail::to($user)->send(new HolidayApproved($data));
+        }
+        if ($record->type === 'decline') {
+            $user = User::find($record->user_id);
+            $data = array(
+                'name' => $user->name,
+                'email' => $user->email,
+                'day' => $record->day,
+            );
+            Mail::to($user)->send(new HolidayApproved($data));
+        }
+
+        return $record;
     }
 }
