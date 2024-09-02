af7d84e8dddeb9550baa7fec98c10115b5e25e7d
Markosgt7 <markos.guate7@gmail.com>
2024-09-02 12:43:12 -0600

    se maneja el dia el nombre y correo para los correos en solicitud de vacaciones

--- app/Filament/Personal/Resources/HolidayResource/Pages/CreateHoliday.php
+++ app/Filament/Personal/Resources/HolidayResource/Pages/CreateHoliday.php
@@ -3,9 +3,12 @@
 namespace App\Filament\Personal\Resources\HolidayResource\Pages;
 
 use App\Filament\Personal\Resources\HolidayResource;
+use App\Mail\HolidayPending;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use App\Models\User;
 
 
 class CreateHoliday extends CreateRecord
@@ -15,6 +18,14 @@ class CreateHoliday extends CreateRecord
     {
         $data['user_id'] = Auth::user()->id;
         $data['type'] = 'pending';
+        $userAdmin = User::find(1);
+        $dataToSend = array(
+            'day' => $data['day'],
+            'name' => User::find($data['user_id'])->name,
+            'email' => User::find($data['user_id'])->email
+        );
+        Mail::to($userAdmin)->send(new HolidayPending($dataToSend));
+
         return $data;
     }
 }
